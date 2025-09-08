7bac86809387b27185e89333bdb32336bf43ed7b
raviprk <raviprk365@gmail.com>
2025-09-08 23:28:13 +1000

    added

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
   trailingSlash: false,
   assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
   basePath: '',
-  output: 'standalone'
 };
 
 module.exports = nextConfig;
