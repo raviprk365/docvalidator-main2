f70cb3af078a10f11b913c97af27d38eac444960
raviprk <raviprk365@gmail.com>
2025-09-08 22:26:01 +1000

    dd

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: false,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+  basePath: '',
   output: 'standalone'
 };
+
 module.exports = nextConfig;
